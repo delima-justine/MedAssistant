b3895903af0eaff58dfb0b42ff0095cd2318a2d3
delima-justine <delimajustine24@gmail.com>
2025-05-29 20:48:12 +0800

    Add changes

--- src/application/Main.java
+++ src/application/Main.java
@@ -10,7 +10,7 @@ public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) {
 		try {
-			Parent root = FXMLLoader.load(getClass().getResource("Sample.fxml"));
+			Parent root = FXMLLoader.load(getClass().getResource("MainDashboard.fxml"));
 			Scene scene = new Scene(root);
 			primaryStage.setTitle("MedAssistant");
 			primaryStage.setScene(scene);
